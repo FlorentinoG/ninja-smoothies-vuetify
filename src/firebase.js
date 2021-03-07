b88a1f6c2c9ede2706e4a0a558ddabef42267880
FlorentinoG9 <f.valle.garcia@gmail.com>
2021-03-07 16:30:07 -0600

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,17 @@
-import * as firebase from 'firebase'
- import 'firebase/firestore'
- 
- // Your web app's Firebase configuration
- var firebaseConfig = {
-     apiKey: "AIzaSyACtICu0CcSdpqZpGIE6R0jfyVKVEs-aRE",
-     authDomain: "ninja-smoothies-e6271.firebaseapp.com",
-     databaseURL: "https://ninja-smoothies-e6271.firebaseio.com",
-     projectId: "ninja-smoothies-e6271",
-     storageBucket: "ninja-smoothies-e6271.appspot.com",
-     messagingSenderId: "1286826335",
-     appId: "1:1286826335:web:246811433e06b47bf06eea"
- };
- // Initialize Firebase
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyACtICu0CcSdpqZpGIE6R0jfyVKVEs-aRE',
+	authDomain: 'ninja-smoothies-e6271.firebaseapp.com',
+	databaseURL: 'https://ninja-smoothies-e6271.firebaseio.com',
+	projectId: 'ninja-smoothies-e6271',
+	storageBucket: 'ninja-smoothies-e6271.appspot.com',
+	messagingSenderId: '1286826335',
+	appId: '1:1286826335:web:246811433e06b47bf06eea',
+};
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
- 
 
-
-export default firebase.firestore()
+export default firebase.firestore();
